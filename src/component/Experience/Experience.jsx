f0cd036e31b5734a429e7b811a91672231079d5c
msadhimb <adhimbaqy72@gmail.com>
2023-05-01 16:16:56 +0700

    add a new experience

--- src/component/Experience/Experience.jsx
+++ src/component/Experience/Experience.jsx
@@ -8,6 +8,7 @@ import abbauf from "./companLogo/abbauf.png";
 import graphie from "./companLogo/graphie.png";
 import udinus from "./companLogo/udinus.png";
 import Len from "./companLogo/Len.png";
+import senatMhsFEBUksw from "./companLogo/senatMahasiswa.PNG";
 import "./Experience.css";
 import sertif from "./assets/sertif.jpg";
 
@@ -15,7 +16,7 @@ const Experience = () => {
   return (
     <section className="experience">
       <div className="container">
-        <h2 className="mt-5">Work Experience</h2>
+        <h2 className="mt-5 mb-3">Work Experience</h2>
         <VerticalTimeline lineColor="#474747" animate={true}>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
@@ -51,15 +52,162 @@ const Experience = () => {
                 PT Len Industri (Persero)
               </h5>
             </div>
-            {/* <h5 className="mt-2 text-start">Assistant</h5>
+          </VerticalTimelineElement>
+
+          {/* UKSW */}
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            dateClassName="text-black text-end"
+            contentArrowStyle={{ borderRight: "7px solid  rgb(46, 209, 84)" }}
+            date="February 2023 - April 2023"
+            iconStyle={{
+              background: "rgb(46, 209, 84)",
+              color: "#fff",
+              width: "30px",
+              height: "30px",
+              marginTop: "15px",
+              marginLeft: "-15px",
+            }}
+          >
+            <h4 className="vertical-timeline-element-title">
+              <strong>Full Stack Web Developer</strong>
+            </h4>
+            <div className="d-flex justify-content-center align-items-center">
+              <img
+                src={senatMhsFEBUksw}
+                alt="graphie-logo"
+                style={{ width: 40 }}
+                className="me-2"
+              />
+              <h5 className="vertical-timeline-element-subtitle">
+                Senat Mahasiswa FEB UKSW
+              </h5>
+            </div>
+
+            <h5 className="mt-2 text-start">Senat Mahasiswa FEB UKSW</h5>
             <div>
               <ul>
                 <li className="text-start">
-                  Help certain lecturer students who experience errors
+                  Developed dashboard admin and dashboard user.
+                </li>
+                <li className="text-start">
+                  Developed dashboard admin and dashboard user using Bootstrap
+                  CSS and React JS.
+                </li>
+                <li className="text-start">
+                  Developed REST API to managed data.
+                </li>
+                <li className="text-start">
+                  Managed to make CRUD using Rest API.
+                </li>
+                <li className="text-start">
+                  Managed to make Login using JWT Token.
+                </li>
+                <li className="text-start">
+                  Push, and pull to git and from git.
+                </li>
+                <li className="text-start">
+                  Managed to host website in RumahWeb Hosting.
+                </li>
+                <li className="text-start">
+                  Successfully work as Full Stack Web Developer to developed
+                  this website.
                 </li>
               </ul>
-            </div> */}
+            </div>
+            <div className="d-flex justify-content-end">
+              <a
+                href="https://senat-mahasiswa-feb-uksw.my.id"
+                style={{
+                  fontWeight: 500,
+                  textDecoration: "none",
+                  color: "green",
+                }}
+              >
+                See Project
+              </a>
+            </div>
           </VerticalTimelineElement>
+
+          {/* Lomba */}
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            dateClassName="text-black text-start"
+            contentArrowStyle={{ borderRight: "7px solid  rgb(46, 209, 84)" }}
+            date="January 2023"
+            iconStyle={{
+              background: "rgb(46, 209, 84)",
+              color: "#fff",
+              width: "30px",
+              height: "30px",
+              marginTop: "15px",
+              marginLeft: "-15px",
+            }}
+          >
+            <h4 className="vertical-timeline-element-title">
+              <strong>
+                Dinus Fest 2023 Information Technology Competition Web
+                Development
+              </strong>
+            </h4>
+            <div className="d-flex justify-content-center align-items-center">
+              <div
+                style={{ background: "white", padding: 2, borderRadius: 50 }}
+                className="me-2"
+              >
+                <img
+                  src={udinus}
+                  alt="graphie-logo"
+                  style={{ width: 25 }}
+                  className="d-block mx-auto"
+                />
+              </div>
+              <h5 className="vertical-timeline-element-subtitle">
+                Universitas Dian Nuswantoro
+              </h5>
+            </div>
+
+            <h5 className="mt-2 text-start">BatikPedia</h5>
+            <div>
+              <ul>
+                <li className="text-start">
+                  Developed dashboard using React JS.
+                </li>
+                <li className="text-start">
+                  Mapping data from JSON data using axios.
+                </li>
+                <li className="text-start">
+                  Push, pull, and merge code using Git.
+                </li>
+                <li className="text-start">
+                  Successfully work as a team to developed a project for
+                  competition.
+                </li>
+              </ul>
+            </div>
+            <div className="d-flex justify-content-end">
+              <a
+                href="https://batik-pedia.vercel.app/user"
+                style={{
+                  fontWeight: 500,
+                  textDecoration: "none",
+                  color: "green",
+                }}
+                className="me-2"
+              >
+                See Project
+              </a>
+              <a
+                href="https://drive.google.com/file/d/1ImLHqTqrtf4Qp1l-61_vWGYGKixrFUYb/view"
+                style={{ fontWeight: 500, textDecoration: "none" }}
+                className="me-2"
+              >
+                See Certificate
+              </a>
+            </div>
+          </VerticalTimelineElement>
+
+          {/* Asisten */}
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             contentArrowStyle={{ borderRight: "7px solid rgb(46,209, 84)" }}
@@ -102,6 +250,8 @@ const Experience = () => {
               </ul>
             </div>
           </VerticalTimelineElement>
+
+          {/* Graphie */}
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             dateClassName="text-black text-start"
@@ -148,6 +298,8 @@ const Experience = () => {
               </ul>
             </div>
           </VerticalTimelineElement>
+
+          {/* Abbauf */}
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             dateClassName="text-black text-end"
